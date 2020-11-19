4f24688c88fc4f3c5c24e8a74f8cde40b37b6172
Shane Jeremich <shane.jeremich@gmail.com>
2020-11-19 14:32:10 -0700

    changed file name

---
+++ 00-Functions.js
@@ -0,0 +1,15 @@
+/*
+Goes through code line-by-line and runes/executes each line - known as the thread of execution
+Saves 'data' like strings and arrays so we can use that data later - in its memory
+*/
+
+const num = 3;
+function multiplyBy2(inputNumber) {
+  const result = inputNumber * 2;
+  return result;
+}
+
+const output = multiplyBy2(num);
+const newOutput = multiplyBy2(10);
+console.log(output);
+console.log(newOutput);
